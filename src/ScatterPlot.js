8ca510226295f80a44bfb18bb23ea59d2d5e447a
Luca Parisi <luca.parisi91@gmail.com>
2020-07-11 10:23:14 +0200

    changed Readme file

---
+++ src/ScatterPlot.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function ScatterPlot() {
+  return (
+    <div className="scatterPlot">
+    </div>
+  );
+}
+
+export default ScatterPlot;
